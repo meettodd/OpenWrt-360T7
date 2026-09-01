f1d8c4b490214c6cf40281ab7cfb85953f3a237e
meettodd <130067102+meettodd@users.noreply.github.com>
2026-09-01 22:17:22 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ###
- # @Author: xiawang1024
- # @Date: 2023-01-06 15:29:51
- # @LastEditTime: 2023-01-06 15:30:17
- # @LastEditors: xiawang1024
+ # @Author: jia
+ # @Date: 2026-09-01 21:00:00
+ # @LastEditTime: 2026-09-01 21:00:00
+ # @LastEditors: jia
  # @Description: 
  # @FilePath: /OpenWrt-360T7/diy2.sh
  # 工作，生活，健康
@@ -20,6 +20,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By Xiaw1024 $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
